5ddbc0a120c414887788639b154171957641b2b3
Andreas Altendorfer <andreas@altendorfer.at>
2018-09-01 19:06:57 +0200

    Upgrade Elixir ~> 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bucketier.MixProject do
     [
       app: :bucketier,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
