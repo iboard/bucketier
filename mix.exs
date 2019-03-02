a80d617cb9b7dc2fc2a08b2d40af961aa2da8c60
Andreas Altendorfer <andreas@altendorfer.at>
2019-03-02 19:07:05 +0100

    Bump vertion for travis

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bucketier.MixProject do
   def project do
     [
       app: :bucketier,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
