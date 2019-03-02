8eaaff7520972e5cb953b35aac3c729f0b1caafa
Andreas Altendorfer <andreas@altendorfer.at>
2019-03-02 20:06:18 +0100

    Fix timing for supervisors in tests

--- test/bucketier_test.exs
+++ test/bucketier_test.exs
@@ -1,11 +1,12 @@
 defmodule BucketierTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Bucketier.Bucket
   alias Bucketier.{Bucket}
 
   setup _ do
-    Bucket.drop_all!()
-    :ok
+    b = Bucket.drop_all!()
+    :timer.sleep(10)
+    {:ok, %{ bucket: b}}
   end
 
   test "get a bucket by name" do
@@ -15,32 +16,32 @@ defmodule BucketierTest do
 
   test "put something in a bucket" do
     bucket =
-      Bucket.bucket("shopping list")
+      Bucket.bucket("shopping list 1")
       |> Bucket.put(1, "Milk")
       |> Bucket.put(2, "Bread")
 
-    assert %Bucket{name: "shopping list", data: %{1 => "Milk", 2 => "Bread"}} == bucket
+    assert %Bucket{name: "shopping list 1", data: %{1 => "Milk", 2 => "Bread"}} == bucket
   end
 
   test "find somthing in a bucket by key" do
     bucket =
-      Bucket.bucket("shopping list")
+      Bucket.bucket("shopping list 3")
       |> Bucket.put(1, "Milk")
       |> Bucket.put(2, "Bread")
       |> Bucket.commit
 
-    assert "Milk" == Bucket.get("shopping list", 1)
-    assert "Bread" == Bucket.get("shopping list", 2)
+    assert "Milk" == Bucket.get("shopping list 3", 1)
+    assert "Bread" == Bucket.get("shopping list 3", 2)
   end
 
   test "bucket persists data on commit" do
-    Bucket.bucket("shopping list")
+    Bucket.bucket("shopping list 4")
     |> Bucket.put(1, "Milk")
     |> Bucket.put(2, "Bread")
     |> Bucket.commit()
 
-    assert %Bucket{name: "shopping list", data: %{1 => "Milk", 2 => "Bread"}} ==
-             Bucket.bucket("shopping list")
+    assert %Bucket{name: "shopping list 4", data: %{1 => "Milk", 2 => "Bread"}} ==
+             Bucket.bucket("shopping list 4")
   end
 
   test "no other bucket is killed if a bucket dies" do
